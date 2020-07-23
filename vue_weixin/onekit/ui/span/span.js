903d8c5e57b76695157dfc2abf5f33e436360151
gongyuan2 <939014677@qq.com>
2020-07-23 11:30:43 +0800

    增加onekit-uihong中标签文件夹

---
+++ vue_weixin/onekit/ui/span/span.js
@@ -0,0 +1,23 @@
+// onekit/ui/span/span.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
