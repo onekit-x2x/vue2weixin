8a2440af1edee83546dac9fe4d81fa8a019c0257
hhm19990929 <65931506+hhm19990929@users.noreply.github.com>
2020-07-24 15:40:17 +0800

    ol li Class\Style

--- vue_weixin/onekit/ui/li/li.js
+++ vue_weixin/onekit/ui/li/li.js
@@ -1,9 +1,14 @@
 // onekit/ui/li/li.js
 Component({
+  options: {
+    styleIsolation: 'apply-shared'
+  },
   /**
    * 组件的属性列表
    */
   properties: {
+    Class:{type:String,value:""},
+    Style:{type:String,value:""},
 
   },
 
--- vue_weixin/onekit/ui/ol/ol.js
+++ vue_weixin/onekit/ui/ol/ol.js
@@ -1,9 +1,15 @@
 // onekit/ui/ol/ol.js
 Component({
+  options: {
+    styleIsolation: 'apply-shared'
+  },
   /**
    * 组件的属性列表
    */
   properties: {
+    Class:{type:String,value:""},
+    Style:{type:String,value:""},
+    value:{type:Number,},
 
   },
 
