160e419b6e0cd839cf6b57cc8dae6e7ad7e5f124
gongyuan2 <939014677@qq.com>
2020-07-24 11:37:26 +0800

    更改vue的video的高宽

--- vue_weixin/onekit/ui/video/video.js
+++ vue_weixin/onekit/ui/video/video.js
@@ -1,8 +1,13 @@
+import {fixurl} from "../../thekit"
 Component({
   options: {
     styleIsolation: 'apply-shared'
   },
   properties: {
+    Style:String,
+    Class:String,
+    width:Number,
+    height:Number,
       src:{
           type: String,
           value: "",
@@ -33,18 +38,43 @@ Component({
       },
   },
    methods: {
-        video_play(e){
-        console.log("video_play", e);
-        this.triggerEvent('play',e.details)
-        },
         video_loadedmetadata(e){
-          console.log("video_loadedmetadata", e);
-          this.triggerEvent('loadedmetadata',e.details)
+            const size = e.detail;
+            var data2 = {};
+            if(!this.properties.width && !this.properties.height){
+                data2.weixin_width = size.width+"px";
+                data2.weixin_height = size.height+"px";
+            }else      if(!this.properties.width && this.properties.height){
+                data2.weixin_width = size.width*this.properties.height/size.height+"px";
+            }else   if(this.properties.width && !this.properties.height){
+                data2.weixin_height = size.height*this.properties.width/size.width+"px";
+                }
+            this.setData(data2);
+        //   console.log("video_loadedmetadata", e);
+        //   this.triggerEvent('loadedmetadata',e.details)
           },
-        video_ended(e){
-        console.log("video_ended", e);
-        this.triggerEvent('ended',e.details)
-        },
+          video_play(e){
+            console.log("video_play", e);
+            this.triggerEvent('play',e.details)
+            },
+            video_ended(e){
+            console.log("video_ended", e);
+            this.triggerEvent('ended',e.details)
+            },
         
-}
+},
+lifetimes: {
+    attached: function() {
+      const pages = getCurrentPages();
+      const currentUrl = pages[pages.length-1].route;
+      const weixin_width =this.properties.width?this.properties.width+"px":"auto";
+      const weixin_height =this.properties.height?this.properties.height+"px":"auto";
+      const weixin_src = "/"+fixurl(currentUrl,this.properties.src);
+      this.setData({weixin_width,weixin_height,weixin_src});
+    },
+    detached: function() {
+     
+    },
+  },
+ 
 });
