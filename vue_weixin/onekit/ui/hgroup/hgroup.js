078293457254e6e7d68ac03c6b2003e3b9532694
lyx0lwz <lyxlwz@qq.com>
2020-09-04 18:02:25 +0800

    v0.1

--- vue_weixin/onekit/ui/hgroup/hgroup.js
+++ vue_weixin/onekit/ui/hgroup/hgroup.js
@@ -1,4 +1,3 @@
-// onekit/ui/hr/hr.js
 import onekit_behavior from "../onekit_behavior"
 import web_behavior from "../web_behavior"
 Component({
@@ -11,17 +10,6 @@ Component({
   properties: {
 
   },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
   methods: {
 
   }
--- vue_weixin/onekit/ui/ol/ol.js
+++ vue_weixin/onekit/ui/ol/ol.js
@@ -3,19 +3,24 @@ import onekit_behavior from "../onekit_behavior"
 import web_behavior from "../web_behavior"
 Component({
   behaviors: [onekit_behavior, web_behavior],
- 
-
+  options: {
+    virtualHost: true,
+  },
   properties: {
    
-    reversed:{type:Boolean,},
+    reversed:{type:Boolean,value:false},
     start:{type:Number,value:1},
     type:{type:String,value:""},
 
   },
   lifetimes: {
     attached: function() {
-      console.log(this.properties.start)
       // 在组件实例进入页面节点树时执行
+      this.properties.start==this.properties.start--;
+      console.log(this.properties.start)
+      this.setData({
+        start: this.properties.start--
+      })
     },
     detached: function() {
       // 在组件实例被从页面节点树移除时执行
