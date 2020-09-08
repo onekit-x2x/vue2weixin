337c34b05bceadbf9154513142a36376a7f9a7e0
gongyuan2 <939014677@qq.com>
2020-09-08 18:20:41 +0800

    v.01

--- vue_weixin/onekit/ui/img/img.js
+++ vue_weixin/onekit/ui/img/img.js
@@ -9,8 +9,8 @@ Component({
     virtualHost: true
   },
   properties: {
-    width:{type:Number,value:""},
-    height:{type:Number,value:""},
+    width:{type:Number|String,valxue:""},
+    height:{type:Number|String,value:""},
     src:{
       type:String,
       value:''
@@ -26,24 +26,37 @@ Component({
    */
   methods: {
     image_load(e){
+      const that = this;
       const size = e.detail;
-      var data2 = {};
-      if(!this.properties.width && !this.properties.height){
-        data2.weixin_width = size.width+"px";
-        data2.weixin_height = size.height+"px";
-      }else      if(!this.properties.width && this.properties.height){
-        data2.weixin_width = size.width*this.properties.height/size.height+"px";
-      }else   if(this.properties.width && !this.properties.height){
-        data2.weixin_height = size.height*this.properties.width/size.width+"px";
-        }
-      this.setData(data2);
+      const query = wx.createSelectorQuery().in(that);
+query.select('.onekit-img').boundingClientRect();
+query.exec(function(res){
+var WIDTH = res[0].width;
+var HEIGHT = res[0].height;
+  
+  var data2 = {};
+  if(!that.properties.width && !that.properties.height){
+
+    data2.weixin_width = size.width+"px";
+    data2.weixin_height = size.height+"px";
+  }else      if(!that.properties.width && that.properties.height){
+    console.log(WIDTH,HEIGHT,size)
+   // data2.weixin_width = size.width*this.properties.height/size.height+"px";
+    data2.weixin_width = size.width*HEIGHT/size.height+"px";
+  }else   if(that.properties.width && !that.properties.height){
+   // data2.weixin_height = size.height*this.properties.width/size.width+"px";
+    data2.weixin_height = size.height*WIDTH/size.width+"px";
+    }
+    that.setData(data2);
+});
+     
     }
   },
   lifetimes: {
     attached: function() {
-      const weixin_width =this.properties.width?this.properties.width+"px":"auto";
-      const weixin_height =this.properties.height?this.properties.height+"px":"auto";
-      var weixin_src = this.properties.src;
+      const weixin_width =this.properties.width?(this.properties.width.indexOf("%")?this.properties.width:(this.properties.width+"px")):"auto";
+      const weixin_height =this.properties.height?(this.properties.height.indexOf("%")?this.properties.height:(this.properties.height+"px")):"auto";
+      var weixin_src = this.properties.src; 
       
      
       if(!weixin_src.indexOf("://")){
