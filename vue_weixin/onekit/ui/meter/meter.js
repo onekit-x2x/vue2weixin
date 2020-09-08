8c0803c854c3320066f9d9e45aecc7ed14833bed
lyx0lwz <lyxlwz@qq.com>
2020-09-08 18:24:40 +0800

    meter

--- vue_weixin/onekit/ui/meter/meter.js
+++ vue_weixin/onekit/ui/meter/meter.js
@@ -9,13 +9,12 @@ Component({
   },
   properties: { 
     value:{type:Number,value:""},
-    max:{type:Number,value:""},
-    min:{type:Number,value:""},
+    max:{type:Number,value:1},
+    min:{type:Number,value:0},
     high:{type:Number,value:""},
     low:{type:Number,value:""},
-    form:{type:String,value:""}
-
-
+    form:{type:String,value:""},
+    percent:{type:Number,value:""}
 
   },
 
@@ -31,5 +30,27 @@ Component({
    */
   methods: {
 
-  }
+  },
+  lifetimes: {
+    attached: function() {
+      // 在组件实例进入页面节点树时执行
+      if (this.properties.max!=1) {
+        this.properties.percent=(this.properties.value-this.properties.min)/(this.properties.max-this.properties.min)*100;
+        console.log(this.properties.percent)
+        this.setData({
+        percent: this.properties.percent
+      })
+      }
+      else{
+        this.properties.percent=(this.properties.value-this.properties.low)/(this.properties.high-this.properties.low)*100;
+        console.log(this.properties.percent)
+        this.setData({
+        percent: this.properties.percent
+      })
+      }
+    },
+    detached: function() {
+      // 在组件实例被从页面节点树移除时执行
+    },
+  },
 })
